b77516d719afc0ff4dd06eca3ff1621041ab8c5b
Viktoriia Rudneva <viktoriiadeveloper@gmail.com>
2024-03-14 14:19:56 +0100

    first commit change basa project Gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,23 @@
 import * as React from 'react';
 
 const pageStyles = {
-  color: '#232129',
+  color: '#2E3638',
   padding: 96,
   fontFamily: '-apple-system, Roboto, sans-serif, serif',
 };
 const headingStyles = {
-  marginTop: 0,
+  marginTop: 20,
   marginBottom: 64,
-  maxWidth: 320,
+  maxWidth: 1320,
 };
 const headingAccentStyles = {
-  color: '#663399',
+  color: '#3E9AB1',
+  width: '1500px',
 };
 const paragraphStyles = {
   marginBottom: 48,
 };
-const codeStyles = {
-  color: '#8A6534',
-  padding: 4,
-  backgroundColor: '#FFF4DB',
-  fontSize: '1.25rem',
-  borderRadius: 4,
-};
+
 const listStyles = {
   marginBottom: 96,
   paddingLeft: 0,
@@ -35,7 +30,7 @@ const listItemStyles = {
 };
 
 const linkStyle = {
-  color: '#8954A8',
+  color: '#1C697D',
   fontWeight: 'bold',
   fontSize: 16,
   verticalAlign: '5%',
@@ -56,112 +51,120 @@ const descriptionStyle = {
 };
 
 const docLink = {
-  text: 'Documentation',
-  url: 'https://www.gatsbyjs.com/docs/',
-  color: '#8954A8',
-};
-
-const badgeStyle = {
-  color: '#fff',
-  backgroundColor: '#088413',
-  border: '1px solid #088413',
-  fontSize: 11,
-  fontWeight: 'bold',
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: '4px 6px',
-  display: 'inline-block',
-  position: 'relative',
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
+  text: 'About me',
+  url: 'https://viktoriiarud.github.io/',
+  color: '#3E9AB1',
 };
 
 const links = [
   {
-    text: 'Tutorial',
-    url: 'https://www.gatsbyjs.com/docs/tutorial/',
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: '#E95800',
+    text: 'Project Vakansiia',
+    url: 'https://project-gatsby-nine.vercel.app',
+    description: '',
+    color: '#EDEB35',
   },
   {
-    text: 'How to Guides',
-    url: 'https://www.gatsbyjs.com/docs/how-to/',
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: '#1099A8',
+    text: 'Restaraunt',
+    url: 'https://restaraunt-9e6fybg6g-rudnevaviktoriia1-gmailcom.vercel.app/',
+    description: "",
+    color: '#EDEB35',
   },
   {
-    text: 'Reference Guides',
-    url: 'https://www.gatsbyjs.com/docs/reference/',
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: '#BC027F',
-  },
-  {
-    text: 'Conceptual Guides',
-    url: 'https://www.gatsbyjs.com/docs/conceptual/',
-    description:
-      'Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.',
-    color: '#0D96F2',
-  },
-  {
-    text: 'Plugin Library',
-    url: 'https://www.gatsbyjs.com/plugins',
-    description:
-      'Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.',
-    color: '#8EB814',
+    text: 'Trip',
+    url: 'https://trip-mu.vercel.app/',
+    description: "",
+    color: '#EDEB35',
   },
 ];
 
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>
-          — you just made a Gatsby site! 🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
+      <main style={pageStyles}>
+        <h1 style={headingStyles}>
+          My name is Viktoriia Rudneva
+          <br/>
           <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
+              style={linkStyle}
+              href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
           >
             {docLink.text}
           </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
+          <br/>
+
+          <br/>
+          <span style={headingAccentStyles}>
+           I am a dedicated and aspiring Front-end Developer with one year
+of professional experience at For Better Future Software House. I
+am pro cient in developing web pages and online stores,
+specializing in React and utilizing additional technologies such as
+Typescript and Gatsby to enhance project functionality.
+Demonstrated versatility by successfully creating an online store
+using Magento 2. I acquired valuable experience as a freelance
+developer by delivering two one-page websites (Landing Pages)
+before joining the company. I am committed to staying abreast of
+industry trends and continually expanding my skills to contribute
+effectively to dynamic development projects.
+        </span>
+        </h1>
+
+        <h2>Work Experience</h2>
+        <h3>For Better Future Soft House</h3>
+        <h4>( February 2023 - Present )</h4>
+        <p>
+          I specialize in React and work on web pages and online stores.
+          <br/>
+          I used design Figma.
+          <br/>
+          Online store project.
+          <br/>
+          I used Magento 2, HTML, Less I did footer, blog, changed styles,
+          changed photos, added products to the store using les csv
+          <h5>(02/2023-05/2023)</h5>
+
+          Project Recruitment website
+          I used React, TypeScript. Created icon components.
+          <h5>(05/2023- 06/2023)</h5>
+          {/*<br/>*/}
+          Project IT Company website
+          I used CSS, StyledComponents, React, TypeScript, and Gatsby. I
+          made components and styles. I created a footer, banner, header,
+          button, tooltip menu, drop down menu, ipping cards, carousel
+          slider, and carousel with reviews. Also, I created basic components,
+          such as a container and theme.
+          <h5>(06/2023...)</h5>
+          <br/>
+          <br/>
+        </p>
+        <h2>Freelancer</h2>
+        <h4>( October 2022 - February 2023 )</h4>
+        <p>
+          Project: “Trip’’ (Landing Page) (10/2022 - 12/2022)
+          <br/>
+          Project: “Restaurant’’ (Landing Page) (12/2022 - 2/2023)
+          <br/>
+          I used technology: CSS, StyledComponents, React, TypeScript</p>
+
+        <p style={paragraphStyles}>
+        </p>
+        <ul style={listStyles}>
+          <li style={docLinkStyle}>
+        <h3>My project</h3>
+          </li>
+          {links.map(link => (
+              <li key={link.url} style={{...listItemStyles, color: link.color}}>
             <span>
               <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
+                  style={linkStyle}
+                  href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
               >
                 {link.text}
               </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
               <p style={descriptionStyle}>{link.description}</p>
             </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
+              </li>
+          ))}
+        </ul>
+      </main>
   );
 };
 
