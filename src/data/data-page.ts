2a29c70cbd04630ee38e3e3f35115403b09ccb5b
Viktoriia Rudneva <viktoriiadeveloper@gmail.com>
2025-01-29 13:14:14 +0100

    change title in experience

--- src/data/data-page.ts
+++ src/data/data-page.ts
@@ -21,18 +21,13 @@ export const dataExperience: ExperienceType = {
     subtitle3: '( February 2023 - Present )',
     subtitle4: ' I specialize in React and work on web pages and online stores.',
     subtitle5: ' I used design Figma.',
-    subtitle6: 'Online store project.',
-    subtitle7: 'I used Magento 2, HTML, Less I did footer, blog, changed styles, changed photos, added products to the store using les csv',
-    subtitle8: '( 02/2023-05/2023 )',
-    subtitle9: '  Project Recruitment website I used React, TypeScript. Created icon components.',
-    subtitle10: '( 05/2023- 06/2023 )',
-    subtitle11: '  Project IT Company website\n' +
-        '                        I used CSS, StyledComponents, React, TypeScript, and Gatsby. I\n' +
-        '                        made components and styles. I created a footer, banner, header,\n' +
-        '                        button, tooltip menu, drop down menu, ipping cards, carousel\n' +
-        '                        slider, and carousel with reviews. Also, I created basic components,\n' +
-        '                        such as a container and theme.',
-    subtitle12: '( 06/2023-04/2024)',
+    subtitle6: 'Online store project : "Housetipster"',
+    subtitle7: 'Creating with a team from scratch a Magento 2. Worked homepage, blog page, product page.',
+    subtitle8: '( 2023 )',
+    subtitle9: 'Project Recruitment website: "Hrk". Creating with a team from scratch using StyledComponents, React, TypeScript, and Gatsby. Worked Components Icons.',
+    subtitle10: '( 2023 )',
+    subtitle11: 'Project IT Company website: Creating with a team from scratch. Using StyledComponents, React, TypeScript, and Gatsby. Worked footer, banner, header, button, tooltip, dropdown menu, flipping cards, carousel, slider, hamburger menu.',
+    subtitle12: '(2024)',
     subtitle13: 'Freelancer',
     subtitle14: '( October 2022 - February 2023 )',
     subtitle15: 'Project: “Trip’’ (Landing Page) (10/2022 - 12/2022)',
